7681635ddee5778fa305cbc53f41d53e65742f95
kasper <kasperbouldin@gmail.com>
2023-05-24 22:23:01 -0700

    Update program 6.1

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	size_t floatSize=sizeof(float);
 	
 	printf("Size of a char: %i byte(s)\n", charSize);
-	printf("Size of a int: %i byte(s)\n", intSize);
+	printf("Size of an int: %i byte(s)\n", intSize);
 	printf("Size of a long int: %i byte(s)\n", longIntSize);
 	printf("Size of a long long int: %i byte(s)\n", longLongIntSize);
 	printf("Size of a float: %i byte(s)\n", floatSize);
