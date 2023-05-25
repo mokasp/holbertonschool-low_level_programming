3c6118bef36050a409379eeddb534089af8b0117
kasper <kasperbouldin@gmail.com>
2023-05-24 22:21:29 -0700

    Update program 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,24 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print size of types'
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	size_t charSize=sizeof(char);
+	size_t intSize=sizeof(int);
+	size_t longIntSize=sizeof(long int);
+	size_t longLongIntSize=sizeof(long long int);
+	size_t floatSize=sizeof(float);
+	
+	printf("Size of a char: %i byte(s)\n", charSize);
+	printf("Size of a int: %i byte(s)\n", intSize);
+	printf("Size of a long int: %i byte(s)\n", longIntSize);
+	printf("Size of a long long int: %i byte(s)\n", longLongIntSize);
+	printf("Size of a float: %i byte(s)\n", floatSize);
+		return (0);
+}
