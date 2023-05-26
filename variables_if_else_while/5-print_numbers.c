fa0eb19c43130a7439481a31f02965bb0aa39490
kasper <kasperbouldin@gmail.com>
2023-05-26 11:14:50 -0700

    Update program 5.1

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'specific digits'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 57)
+	{
+		putchar(x);
+		++x;
+	}
+	printf("\n");
+	return (0);
+}
