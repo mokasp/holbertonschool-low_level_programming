c63b487cb05010f921f8cac2fbeba3f3bcd0c5e2
kasper <kasperbouldin@gmail.com>
2023-05-26 10:53:19 -0700

    Update script 3.2

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,22 @@ int main(void)
 {
 	int x = 97;
 
-	while (x <= 122)
+	while (x >= 97 && x <= 122)
 	{
 		putchar(x);
-		x++;
-		printf("\n");
+		++x;
+
+		if (x >= 123)
+		{
+			x = 65;
+		}
+
+		while (x >= 65 && x <= 90)
+		{
+			putchar(x);
+			++x;
+		}
 	}
+	printf("\n");
 	return (0);
 }
