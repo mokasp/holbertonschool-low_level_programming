9c1b3a69d9b4f82cf64bf383318b81fee59beddb
kasper <kasperbouldin@gmail.com>
2023-05-29 09:57:15 -0700

    Update script 1.2

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Description: 'alphabet'
  *
  * Return: Always 0 (Success)
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
+	int x = 97;
+
+	while (x <= 122)
+	{
+		_putchar(x);
+		x++;
+	}
 	_putchar('\n');
-	return (0);
+	return;
 }
