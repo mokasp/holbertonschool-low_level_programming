fe6c632223c1f72fadae669c4fdd018ab359cf37
kasper <kasperbouldin@gmail.com>
2023-05-29 09:23:22 -0700

    Update program 1.1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'alphabet'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
