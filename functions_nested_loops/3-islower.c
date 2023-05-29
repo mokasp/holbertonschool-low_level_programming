e2c6b79f11ba4375d25b8bea109e65c0c9dc6151
kasper <kasperbouldin@gmail.com>
2023-05-29 14:32:00 -0700

    Update program 3.1

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - check if lower case
+ *
+ * @c: checks if lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
