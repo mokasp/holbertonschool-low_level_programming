027667ee65bdf9889523cf463e69a8840acfb991
kasper <kasperbouldin@gmail.com>
2023-05-30 02:49:02 -0700

    Update program 9.1

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * times_table - 9's times table
+ *
+ * Return: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+	int x, y, w, z, q;
+
+	x = 0;
+
+	while (x <=  9)
+	{
+		y = 0;
+		while (y <= 9)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				w = z % 10;
+				q = z / 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(q + '0');
+				_putchar(w + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(z + '0');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
