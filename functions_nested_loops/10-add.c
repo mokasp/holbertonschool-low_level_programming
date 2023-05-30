f8679cab2db3ac5552daf5edead79c82eaccf119
kasper <kasperbouldin@gmail.com>
2023-05-30 03:21:38 -0700

    Update progran 10.1

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: numbers to add
+ * @b: numbers to add
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
