85a1535cd9c23f688b61e188346b1158b68388d3
kasper <kasperbouldin@gmail.com>
2023-05-31 13:00:39 -0700

    Update program 10.1

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @n: number of hashtags
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = x; y < size; y++)
+			{
+				_putchar(' ');
+			}
+			for(z = 1; z <= x; z++)
+			{
+				_putchar('#');
+			}
+			_putchar(10);
+		}
+	}
+}
