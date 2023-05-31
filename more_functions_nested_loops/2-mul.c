ef6f9690f88e86de236f7b58e5b0c1378ced9c0e
kasper <kasperbouldin@gmail.com>
2023-05-31 09:11:06 -0700

    Update program 2.1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiply integers
+ * @a: character to be multiplied
+ * @b: character to be multiplied
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
+
