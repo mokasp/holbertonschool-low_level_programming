9993a7936242008962490153dbe1302ed0a51b06
kasper <kasperbouldin@gmail.com>
2023-06-01 13:28:46 -0700

    Update program 0.1

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -- update value of a pointer
+ * @n: value to be updated
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+	_putchar(*n);
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning pointers strings and arrays
