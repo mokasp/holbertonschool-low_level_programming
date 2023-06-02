d3315fa1429f73c14b8eaf4d7688b98f0a972110
kasper <kasperbouldin@gmail.com>
2023-06-02 16:47:41 -0700

    Update program 9.4

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i, len;
 
-	for (i = 0; src[i]; ++i)
+	len = 0;
+
+	while (src[len])
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
