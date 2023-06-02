b237a9ea4c9dff32836d9d5f7f3b96071ba027c1
kasper <kasperbouldin@gmail.com>
2023-06-02 16:38:10 -0700

    Update program 9.1

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy string
+ * @dest: new string
+ * @src: original string
+ *
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; ++i)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
