4cb0e78d954baac5cbc663f59cf5aefba234afb4
kasper <kasperbouldin@gmail.com>
2023-06-05 21:15:02 -0700

    update program 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,7 +2,7 @@
 
 /**
  * _atoi - convert string to integer
- * @s: 
+ * @s: string
  *
  * Return: Always 0.
  */
@@ -20,34 +20,20 @@ int _atoi(char *s)
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-		{
 			if (x < 0)
-			{
 				x = -x;
-			}
 			else
-			{
 				x--;
-			}
-		}
 		if (s[i] >= '0' && s[i] <= '9')
-		{
 			y = s[i] - 48;
 			z = z * 10 + y;
 			i++;
-		}
 		else if (z == 0)
-		{
 			i++;
-		}
 		else
-		{
 			break;
-		}
 	}
 	if (x < 0)
-	{
 		z = -z;
-	}
 	return (z);
 }
