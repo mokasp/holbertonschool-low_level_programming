dc5ffb79d7a46c2a1acfaabe46e0bced2f549b17
kasper <kasperbouldin@gmail.com>
2023-06-05 21:11:15 -0700

    Update program 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,37 +9,45 @@
 
 int _atoi(char *s)
 {
-	int i, len, w, x, y, z;
+	int i, x, y;
+	unsigned int z;
 
 	i = 0;
-	len = 0;
-	w = 0;
 	x = 0;
 	y = 0;
 	z = 0;
 
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && y == 0)
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			++w;
-
+		{
+			if (x < 0)
+			{
+				x = -x;
+			}
+			else
+			{
+				x--;
+			}
+		}
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			z = s[i] - '0';
-			if (w % 2)
-				z = -z;
-			x = x * 10 + z;
-			y = 1;
-			if (s[i + 1] <= '0' || s[i + 1] >= '9')
-				break;
-			y = 0;
+			y = s[i] - 48;
+			z = z * 10 + y;
+			i++;
 		}
-		i++;
+		else if (z == 0)
+		{
+			i++;
+		}
+		else
+		{
+			break;
+		}
 	}
-	if (y == 0)
-		return (0);
-	return (x);
+	if (x < 0)
+	{
+		z = -z;
+	}
+	return (z);
 }
