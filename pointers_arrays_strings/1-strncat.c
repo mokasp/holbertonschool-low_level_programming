d7c0a5c7fcacfe799f5be4c1cba863fffa76bf42
mokasp <kasperbouldin@gmail.com>
2023-06-08 10:16:50 -0700

    Update program 01.2

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -4,6 +4,7 @@
  * *_strncat - concatenate two strings
  * @dest: second string
  * @src: first string
+ * @n: size of src
  *
  * Return: Always 0.
  */
@@ -11,14 +12,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-	
-	for (i = 0; dest[i] != '\0'; i++);
-	
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[i + j] = src[j];
 	}
-	
 	dest[i + j] = '\0';
 
 	return (dest);
