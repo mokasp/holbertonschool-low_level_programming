7ac5e87b2c1731aafd52a18b34611c40590eb72e
mokasp <kasperbouldin@gmail.com>
2023-06-08 15:05:41 -0700

    Update program 03.2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,23 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int x;
+	int x, y, i;
 
-	x = s1[0] - s2[0];
-
-	if (x < 0)
-		return (x);
-	else if (x > 0)
-		return (x);
-	else
-		return (0);
+	for (i = 0; s1[i]; i++)
+	{
+		for (x = 0; s2[x]; x++)
+		{
+			if (s1[i] != s2[x])
+			{
+				y = s1[i] - s2[x];
+				return (y);
+				break;
+			}
+			else
+			{
+				return (0);
+			}
+		}
+	}
+	return (0);
 }
