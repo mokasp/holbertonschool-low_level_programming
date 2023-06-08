a698209e8e3ad528a1dceafe26efcfd1f0fc2dfa
mokasp <kasperbouldin@gmail.com>
2023-06-08 10:12:55 -0700

    Update program 01.1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,9 +11,9 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-
+	
 	for (i = 0; dest[i] != '\0'; i++);
-
+	
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[i + j] = src[j];
@@ -21,5 +21,5 @@ char *_strncat(char *dest, char *src, int n)
 	
 	dest[i + j] = '\0';
 
-	return dest;
+	return (dest);
 }
