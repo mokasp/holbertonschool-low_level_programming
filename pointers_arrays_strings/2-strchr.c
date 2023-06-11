d70290088f938740273e24be2803545f0636afe5
mokasp <kasperbouldin@gmail.com>
2023-06-11 11:53:23 -0700

    Update program 002

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strchr - searches the first occurence of char c
+ * @s: string
+ *
+ * Return: Always 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c && *s != '\0')
+	{
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return NULL;
+	}
+}
