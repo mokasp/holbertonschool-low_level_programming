f39c4d0b28229a6b2b0d7f6baa50274bdfbeed96
mokasp <kasperbouldin@gmail.com>
2023-06-12 19:19:33 -0700

    Fix deleted file

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * *_strstr - return first occurance of substring in a string
  * @haystack: initial string to be searched
- * @needle: substirng
+ * @needle: substring
  *
  * Return: Always 0.
  */
@@ -15,13 +14,13 @@ char *_strstr(char *haystack, char *needle)
 	{
 		char *hay = haystack;
 		char *need = needle;
-
+		
 		while (*hay == *need && *need != '\0')
 		{
 			hay++;
 			need++;
 		}
-
+		
 		if (*need == '\0')
 		{
 			return (haystack);
@@ -29,3 +28,4 @@ char *_strstr(char *haystack, char *needle)
 	}
 	return (NULL);
 }
+
