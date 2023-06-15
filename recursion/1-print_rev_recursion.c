3c9bab8a902e78f915ba4dc5acb5b1088382bd69
mokasp <kasperbouldin@gmail.com>
2023-06-15 15:52:14 -0700

    Update program 001

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,10 +1,19 @@
 #include "main.h"
 
 /**
- * ~ - ~
- * @~: ~
+ * _print_rev_recursion - print reverse of string w recursion
+ * @s: string
  *
  * Return: Always 0 (Success)
  */
 
-
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(10);
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
