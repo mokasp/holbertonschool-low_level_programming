b037c7212392fd74c16233117f8616d992093258
mokasp <kasperbouldin@gmail.com>
2023-06-16 10:23:45 -0700

    Update program 3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,10 +1,24 @@
 #include "main.h"
 
 /**
- * ~ - ~
- * @~: ~
+ * factorial - return the factorial of a given number
+ * @n: number
  *
  * Return: Always 0 (Success)
  */
 
-
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n >= 1)
+	{
+		return (n * factorial(n - 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
