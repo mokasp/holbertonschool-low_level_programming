4e482f89074232066bfea2e114318269412678ce
mokasp <kasperbouldin@gmail.com>
2023-06-16 14:18:06 -0700

    Update program 6

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
 /**
- * ~ - ~
- * @~: ~
+ * _sqrt_recursion - find square root of a number
+ * @n: number
  *
  * Return: Always 0 (Success)
  */
 
+int _sqrt_recursion(int n)
+{
+	int x, y;
 
+	int x = 
+}
--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,10 +1,38 @@
 #include "main.h"
 
 /**
- * ~ - ~
- * @~: ~
+ * helper - helps find prime number
+ * @n: number
+ * @i: 2
  *
  * Return: Always 0 (Success)
  */
 
+int helper(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (helper(n, i + 1));
+}
 
+/**
+ * is_prime_number - find if number is prime
+ * @n: number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (helper(n, 2));
+}
