d870bcb4869e894ed96aa8104d0358beb35a2514
mokasp <kasperbouldin@gmail.com>
2023-06-16 12:24:54 -0700

    Update program 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,10 +1,25 @@
 #include "main.h"
 
 /**
- * ~ - ~
- * @~: ~
+ * _pow_recursion - calculate power of a number
+ * @x: base number
+ * @y: exponent
  *
  * Return: Always 0 (Success)
  */
 
-
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
