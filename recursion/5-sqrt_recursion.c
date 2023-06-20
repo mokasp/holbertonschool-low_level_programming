2f86bff6a0a983d4c22984f09b5835f4b2120609
mokasp <kasperbouldin@gmail.com>
2023-06-19 19:41:09 -0700

    Update program 5.1

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,15 +1,44 @@
 #include "main.h"
+/**
+ * helper - helps find square root
+ * @n: number
+ * @i: counter/root
+ *
+ * Return: Always 0 (Success)
+ */
 
+int helper(int n, int i)
+{
+	if ((i * i) == n)
+	{
+		return (i);
+	}
+
+	if (i == n / 2)
+	{
+		return (-1);
+	}
+
+	return (helper(n, i + 1));
+}
 /**
  * _sqrt_recursion - find square root of a number
  * @n: number
  *
  * Return: Always 0 (Success)
  */
-
 int _sqrt_recursion(int n)
 {
-	int x, y;
+	int i = 0;
 
-	int x = 
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+
+	return (helper(n, i));
 }
