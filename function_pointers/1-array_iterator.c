9f93b60dce8233254c2b4d79ff4bd803b471cd2c
mokasp <kasperbouldin@gmail.com>
2023-07-01 15:19:44 -0700

    1.1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - uses function as parameter on elements of array
+ * @array: array to use
+ * @size: size of array
+ * @action: pointer to function to use as parameter
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	while (*array)
+		action(size);
+}
+
