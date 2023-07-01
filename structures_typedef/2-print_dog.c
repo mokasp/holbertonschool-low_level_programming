58f7132431b71c952e1e0f3ddf21cf17424f4732
mokasp <kasperbouldin@gmail.com>
2023-07-01 10:22:05 -0700

    2.13

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,9 +18,8 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 	else
 		printf("Name: (nil)\n");
-
-	if (d->age)
-		printf("Age: %f\n", d->age);
+	
+	printf("Age: %f\n", d->age);
 
 	if (d->owner)
 		printf("Owner: %s\n", d->owner);
