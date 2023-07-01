f4f8ac45b1fdaf1df791e6968e9dcc63cf79094f
mokasp <kasperbouldin@gmail.com>
2023-07-01 12:47:45 -0700

    4.22

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -58,8 +58,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	new_dog->name = _strdup(name);
+	free(name);
 	new_dog->age = age;
 	new_dog->owner = _strdup(owner);
+	free(owner);
 
 	free(new_dog);
 
