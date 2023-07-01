a186620baee194a76332b1605c6b031d2d0c6fcf
mokasp <kasperbouldin@gmail.com>
2023-07-01 09:21:07 -0700

    hi

--- structures_typedef/pushtogit.sh
+++ structures_typedef/pushtogit.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+echo 'Enter the file name you want to add:'
+read fileName
 
-# lines that start like this are shell comments
-# read projects current directory with $PWD
-echo “running command from” $PWD
-cd $PWD
-git add .
-echo “Enter commit message: “
-git commit -am “$commitMessage”
-git push
+git add "$fileName"
+
+echo 'Enter the commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+echo 'Enter the name of the branch:'
+read branch
+
+git push origin $branch
+
+read
