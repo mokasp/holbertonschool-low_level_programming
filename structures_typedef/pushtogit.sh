66ad0b181827159d9970f87521223c3bc46cfc4b
mokasp <kasperbouldin@gmail.com>
2023-07-01 09:21:57 -0700

    hi

--- structures_typedef/pushtogit.sh
+++ structures_typedef/pushtogit.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-echo 'Enter the file name you want to add:'
-read fileName
 
-git add "$fileName"
+git add .
 
 echo 'Enter the commit message:'
 read commitMessage
 
 git commit -m "$commitMessage"
 
-echo 'Enter the name of the branch:'
-read branch
-
-git push origin $branch
+git push
 
 read
