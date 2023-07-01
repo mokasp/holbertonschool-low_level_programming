e811b8529441dadc2c04cda6eb735294ea889f8c
mokasp <kasperbouldin@gmail.com>
2023-07-01 12:21:14 -0700

    4.15

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,43 @@
 #include "dog.h"
 
 /**
+ * _strdup - duplicate a string
+ * @elmnt: element to duplicate
+ *
+ * Return: duplicated string
+ */
+
+char *_strdup(char *elmt)
+{
+	char *dup;
+	char *p;
+	int len = 0;
+
+	if (elmt == NULL)
+		return (NULL);
+
+	while (elmt[len])
+		len++;
+
+	dup = (char *)malloc(sizeof(char) * len + 1);
+
+	if (dup == NULL)
+		return (NULL);
+
+	p = dup;
+
+	while (*elmt)
+	{
+		*p = *elmt;
+		p++;
+		elmt++;
+	}
+	*p = '\0';
+
+	return (dup);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
@@ -19,9 +56,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	new_dog->name = name;
+	new_dog->name = _strdup(name);
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = _strdup(owner);
 
 	return (new_dog);
 }
