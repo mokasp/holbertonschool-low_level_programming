b492a9c5a16493f041c95e9d5d299aef58eaaa21
mokasp <kasperbouldin@gmail.com>
2023-07-02 12:41:40 -0700

    0.3

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,14 +3,16 @@
 /**
  * sum_them_all - adds together all parameters
  * @n: number of parameters
+ *
+ * Return: sum of all parameters
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int sum = 0;
 	unsigned int i;
-
 	va_list ptr;
+
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, unsigned int);
