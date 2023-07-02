fde27165ae27224bb55cea7d022dfdd3717db0b2
mokasp <kasperbouldin@gmail.com>
2023-07-02 13:06:32 -0700

    1.5

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,6 +16,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ptr, n);
 
+	if (separator == NULL)
+		separator = "";
+
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(ptr, unsigned int);
