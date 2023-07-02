a7a54e39121ecd998bb4b29521951f4e1091177d
mokasp <kasperbouldin@gmail.com>
2023-07-02 12:59:21 -0700

    1.1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,6 +1,11 @@
 #include "variadic_functions.h"
 
 /**
+ * print_numbers - print numbers
+ * @separator: string between numbers
+ * @n: number of arguments
+ *
+ * Return: void
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -8,13 +13,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int x;
 	unsigned int i;
 	va_list ptr;
+
+	separator = ", ";
 	
 	va_start(ptr, n);
 
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(ptr, unsigned int);
-		printf("x: %d\n", x);
+		printf("x: %d%c", x, separator);
 	}
 	va_end(ptr);
 }
