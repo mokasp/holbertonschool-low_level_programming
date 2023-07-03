8f7256bdb1bb8da7726c0f5e6e159021e9e90b60
mokasp <kasperbouldin@gmail.com>
2023-07-03 16:51:19 -0700

    3.36

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((num2 == 0) && (&argv[2] == '/' || &argv[2] == '%'))
+	if ((num2 == 0) && (argv[2] == "/" || argv[2] == "%"))
 	{
 		printf("Error\n");
 		exit(100);
