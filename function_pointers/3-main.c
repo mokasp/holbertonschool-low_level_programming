7c7294982b10ff2420f4305fe50166b57475eaf4
mokasp <kasperbouldin@gmail.com>
2023-07-03 16:46:22 -0700

    3.33

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((num2 == 0) && (op == ops[4].f || op == ops[5].f))
+	if ((num2 == 0) && (op == *(ops[4].f) || op == *(ops[5].f))
 	{
 		printf("Error\n");
 		exit(100);
