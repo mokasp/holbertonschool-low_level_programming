e294393b30190136773c673a893e3bf1ad78f738
mokasp <kasperbouldin@gmail.com>
2023-07-03 19:44:19 -0700

    3.idk

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,5 +1,88 @@
 #include "variadic_functions.h"
 
 /**
+ * print_all - prints any type
+ * @format: list of arguments
+ *
+ * Return: void
  */
 
+void print_all(const char * const format, ...)
+{
+	form_t type[] = {
+		{'i', integer},
+		{'c', character},
+		{'f'', flt},
+		{'s', *str},
+		{'\0', NULL}
+	};
+	int i, j;
+	char *sep;
+	va_list hm;
+
+	sep = "",
+	i = 0;
+	j = 0;
+
+	va_start(hm, format);
+
+	while (format && format[i]);
+	{
+		while (type[j].w)
+		{
+			if (format[i] == type[j].w)
+			{
+				printf("%s", sep);
+				type[j].f(hm);
+				sep = ", ";
+			}
+			++j;
+		}
+		j = 0;
+		i++;
+	}
+	va_end(hm);
+}
+
+
+/**
+ *
+ *
+ *
+ */
+void integer(va_list hm)
+{
+	printf("%d", va_arg(ptr, int));
+}
+
+/**
+ *
+ *
+ *
+ */
+void character(va_list)
+{
+	printf("%c", va_args(ptr, char));
+}
+/*
+ *
+ *
+ *
+ */
+void flt(va_list hm)
+{
+	printf("%f", va_args(ptr, float));
+}
+
+/**
+ *
+ *
+ *
+ */
+void *str(va_list hm)
+{
+	if (s == NULL)
+		printf("(nil");
+	
+	printf("%s", va_args(ptr, char *));
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,11 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+/**
+ * struct type - Struct type
+ *
+ * @type: the type
+ * @f: the function associated
+ */
+
+typedef struct w
+{
+	char w;
+	char(*f)(char *separator, va_list args);
+} form_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+void integer(va_list hm);
+void character(va_list hm);
+void flt(va_list hm);
+void *str(va_list hm);
 
 #endif
