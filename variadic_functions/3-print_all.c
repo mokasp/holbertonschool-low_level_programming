f72c527c76e2d876267671561af1d88e99b5c879
mokasp <kasperbouldin@gmail.com>
2023-07-03 19:54:35 -0700

    idk

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,51 +1,6 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints any type
- * @format: list of arguments
- *
- * Return: void
- */
-
-void print_all(const char * const format, ...)
-{
-	form_t type[] = {
-		{"i", integer},
-		{"c", character},
-		{"f"', flt},
-		{"s", *str},
-		{NULL, NULL}
-	};
-	int i, j;
-	char *sep;
-	va_list hm;
-
-	sep = "",
-	i = 0;
-	j = 0;
-
-	va_start(hm, format);
-
-	while (format && format[i]);
-	{
-		while (type[j].w)
-		{
-			if (format[i] == type[j].w)
-			{
-				printf("%s", sep);
-				type[j].f(hm);
-				sep = ", ";
-			}
-			++j;
-		}
-		j = 0;
-		i++;
-	}
-	va_end(hm);
-}
-
-
-/**
  *
  *
  *
@@ -85,4 +40,48 @@ void *str(va_list hm)
 		printf("(nil");
 	
 	printf("%s", va_args(ptr, char *));
+}
+
+/**
+ * print_all - prints any type
+ * @format: list of arguments
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	form_t type[] = {
+		{"i", integer},
+		{"c", character},
+		{"f"', flt},
+		{"s", *str},
+		{NULL, NULL}
+	};
+
+	int i, j;
+	char *sep;
+	va_list hm;
+	
+	sep = "",
+	i = 0;
+	j = 0;
+	
+	va_start(hm, format);
+
+	while (format && format[i]);
+	{
+		while (type[j].w)
+		{
+			if (format[i] == type[j].w)
+			{
+				printf("%s", sep);
+				type[j].f(hm);
+				sep = ", ";
+			}
+			++j;
+		}
+		j = 0;
+		i++;
+	}
+	va_end(hm);
 }
