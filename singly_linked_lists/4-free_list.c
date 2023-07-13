9f1027f78234f0c6d5dc74ebc26e2215ce9be143
mokasp <kasperbouldin@gmail.com>
2023-07-13 10:16:36 -0700

    initial file commit

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: pointer to first element
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head);
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
