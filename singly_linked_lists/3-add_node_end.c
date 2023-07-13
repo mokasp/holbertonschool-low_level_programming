880035497503277788e68cfcac3ecda78414a292
mokasp <kasperbouldin@gmail.com>
2023-07-13 10:07:45 -0700

    changed return

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -31,7 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return;
+		return (new_node);
 	}
 	while (last->next != NULL)
 		last = last->next;
