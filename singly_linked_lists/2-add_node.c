105dbad45cc38cc44383d3fa741bbe59540e72a7
mokasp <kasperbouldin@gmail.com>
2023-07-12 23:27:55 -0700

    fixed strdup

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,12 +13,13 @@ list_t *add_node(list_t **head, const char *str)
 	list_t **new_head;
 	struct list_t *new_node;
 	
-	new_node = strdup(str);
-	free(new_node);
+	new_node->str = strdup(str);
+	free(new_node->str);
 
 	if (new_node == NULL)
 		return (NULL);
 
-	new_head = &new_node
+	new_head = &new_node;
+
 	return (head);
 }
