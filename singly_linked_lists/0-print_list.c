40f2aa162c990e9ba499b4f013a87802e3112885
mokasp <kasperbouldin@gmail.com>
2023-07-12 21:18:59 -0700

    null behavior

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,10 +14,14 @@ size_t print_list(const list_t *h)
 	node_cnt = 0;
 
 	if (h == NULL)
+	{
 		return (0);
 
 	if (h->str == NULL)
+	{
 		printf("[%d] %s\n", 0, "(nil)");
+		h = h->next;
+	}
 
 	while (h != NULL)
 	{
