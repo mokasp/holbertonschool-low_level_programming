9c68dfce826d347c2b54d64966010185ddbc7ce1
mokasp <kasperbouldin@gmail.com>
2023-07-13 10:04:57 -0700

    added while loop

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -29,7 +29,13 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = new_len;
 	new_node->next = NULL;
 	if (*head == NULL)
+	{
 		*head = new_node;
+		return;
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
 
 	return (new_node);
 }
