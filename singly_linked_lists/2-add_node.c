1d45d3b2ea0993b7399064ec54d6c939ca5cbd12
mokasp <kasperbouldin@gmail.com>
2023-07-13 09:22:44 -0700

    added new int variable

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,7 +12,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *new_str;
-
+	unsigned int new_len;
+	
+	new_len = strlen(str);
 	new_node = (list_t *) malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -22,7 +24,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_str);
 		free(new_node);
 	}
-	new_node->len = strlen(str);
+	new_node->len = new_len;
 	new_node->next = (*head);
 
 	return (new_node->next);
