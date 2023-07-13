1d6eb5fb8dc83ca05c97d6bb36d746a6c8f09914
mokasp <kasperbouldin@gmail.com>
2023-07-12 20:18:40 -0700

    added header, readme, and inital pfile

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "list.h"
+
+/**
+ * print_list: prints all elements of a singly linked list
+ * @h: singly linked list
+ *
+ * Return: number of nudes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int node_cnt;
+
+	node_cnt = 0;
+
+	while (head != NULL)
+	{
+		if (str == NULL)
+			printf("[0] (nil)");
+		printf("[%d] %s\n", head->len, head->str);
+		head->next;
+		node_cnt++
+	}
+	return (node_cnt);
+}
