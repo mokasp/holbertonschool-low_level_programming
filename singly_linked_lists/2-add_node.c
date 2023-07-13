fdfff5ced8b9353406829524b9002a88c043114b
mokasp <kasperbouldin@gmail.com>
2023-07-13 09:45:52 -0700

    typo

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -27,7 +27,7 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = new_str;
 	new_node->len = new_len;
 	new_node->next = (*head);
-	*head = new
+	*head = new_node;
 
 	return (head);
 }
