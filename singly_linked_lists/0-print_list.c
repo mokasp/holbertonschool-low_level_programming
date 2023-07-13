3312f26fb9c6bcbdb5f7d70daccdff41eeb76a3b
mokasp <kasperbouldin@gmail.com>
2023-07-12 21:35:49 -0700

    betty editing

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -17,8 +17,10 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
+		
 		else
 			printf("[%d] %s\n", h->len, h->str);
+		
 		node_cnt++;
 		h = h->next;
 	}
