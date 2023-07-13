fd5c9885a90f698d6a5eda2dfc255ff7ed7be8b1
mokasp <kasperbouldin@gmail.com>
2023-07-13 09:19:30 -0700

    added new string variablle

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,18 +11,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	char new_str;
 
 	new_node = (list_t *) malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	new_str = strdup(str);
+	if (new_str == NULL)
 	{
-		free(new_node->str);
+		free(new_str);
 		free(new_node);
 	}
 	new_node->len = strlen(str);
-	new_node->next = *head;
+	new_node->next = (*head);
 
 	return (new_node->next);
 }
