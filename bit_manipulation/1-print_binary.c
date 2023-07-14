418e08a4f02650cbcb48ac899911079bef837a6d
mokasp <kasperbouldin@gmail.com>
2023-07-14 12:29:00 -0700

    initial file commit

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of a number
+ * @n: number to convert
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+
+	for (i = << 63; i >0; i = i /2)
+		(n & i) ? _putchar("1") : _putchar("0");
+}
