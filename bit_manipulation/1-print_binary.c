80a17494fc372cbe4c0bc405dc10fb4227f8e632
mokasp <kasperbouldin@gmail.com>
2023-07-14 12:56:18 -0700

    typo

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -12,5 +12,5 @@ void print_binary(unsigned long int n)
 	unsigned long int i;
 
 	for (i = 1UL << 63; i > 0; i = n >> 1)
-		(n & i) ? printf('1') : printf("0");
+		(n & i) ? printf("1") : printf("0");
 }
