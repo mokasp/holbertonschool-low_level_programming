74ca06c49737f53ca7cc822b8c3e1760a30aafba
mokasp <kasperbouldin@gmail.com>
2023-07-14 13:51:16 -0700

    intial file commit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of a bit at a given index value 
+ * @n: number
+ * @index: index
+ *
+ * Return: void
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bit;
+
+	bit = (n >> i) & 1;
+
+	return (bit);
+}
