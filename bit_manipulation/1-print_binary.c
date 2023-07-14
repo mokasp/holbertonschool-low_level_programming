ebb2610f3da1bedfd61e95846b1c76734b8baa2c
mokasp <kasperbouldin@gmail.com>
2023-07-14 13:23:28 -0700

    testing

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,8 +9,8 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int i;
-
-	for (i = 1UL << 63; i > 0; i = n >> 1)
-		(n & i) ? printf("1") : printf("0");
+	if (n > 1)
+		print_binary(n >> 1);
+	
+	printf("%lu", n & 1);
 }
