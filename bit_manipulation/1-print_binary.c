0ec6e231a8d06815c03862ea0ebf9ec52c99002e
mokasp <kasperbouldin@gmail.com>
2023-07-14 12:38:30 -0700

    unsigned long?

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -11,6 +11,6 @@ void print_binary(unsigned long int n)
 {
 	unsigned long int i;
 
-	for (i = << 31; i > 0; i = i /2)
+	for (i = 1UL << 63; i > 0; i = i /2)
 		(n & i) ? printf("1") : printf("0");
 }
