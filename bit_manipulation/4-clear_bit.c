61fcef016c93e628eb6bdf2b0a70ecc1adfbce45
mokasp <kasperbouldin@gmail.com>
2023-07-14 15:21:48 -0700

    initial push

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * set_bit - sets value of a bit at a given index value to 1
+ * @n: pointer to number
+ * @index: index
+ *
+ * Return: void
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int bit;
+
+	if (index > 63)
+		return (-1);
+
+	bit = 1 << index;
+	if (*n & bit)
+		*n ^= bit;
+
+	return (1);
+}
