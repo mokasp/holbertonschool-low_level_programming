c9047a991137dbfe25d3c4685327e6246386e41b
mokasp <kasperbouldin@gmail.com>
2023-07-15 10:19:14 -0700

    another main typo

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -11,7 +11,7 @@ void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-int flip_bits(unsigned long int n, unsigned int m);
+unsigned int flip_bits(unsigned long int n, unsigned int m);
 int _putchar(char c);
 unsigned int count_bits(unsigned int x);
 
