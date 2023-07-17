d796a61654e5708944c427810502b845e43dc33b
mokasp <kasperbouldin@gmail.com>
2023-07-17 13:43:38 -0700

    typo

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -14,7 +14,7 @@ int pop_listint(listint_t **head)
 
 	if ((*head) == NULL)
 		return (0);
-	date = (*head)->n;
+	data = (*head)->n;
 	if ((*head)->next != NULL)
 	{
 		(*head)->n = (*head)->next->n;
