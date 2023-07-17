524aa0c2c14d0b213b2b5c70b298a99026ff3aec
mokasp <kasperbouldin@gmail.com>
2023-07-16 22:58:34 -0700

    typo

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -8,7 +8,7 @@
  * Return: address of the new element
  */
 
-listint_t *add_nodeint(listint_t **head, const int *n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 	int *new_int;
