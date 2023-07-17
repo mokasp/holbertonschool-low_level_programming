c39292d24e2ba88910ebbcaad2826352e5ec5154
mokasp <kasperbouldin@gmail.com>
2023-07-17 13:33:59 -0700

    moved around

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -12,11 +12,8 @@ int pop_listint(listint_t **head)
 	int data;
 	listint_t *temp;
 
-	data = 0;
-	if ((*head) == NULL)
+	if ((*head) == NULL || (*head)->next == NULL)
 		return (0);
-	if ((*head)->next == NULL)
-		return(data);
 	data = (*head)->n;
 	(*head)->n = (*head)->next->n;
 	temp = (*head)->next;
