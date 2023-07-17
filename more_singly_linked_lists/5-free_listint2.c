13a1e79ed5a2d6fd39c06b1bbab26e62b5373b9e
mokasp <kasperbouldin@gmail.com>
2023-07-16 23:12:46 -0700

    Initial file commit

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to first element
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t *head)
+{
+	listint_t *current;
+	listint_t *tmp;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		tmp = current;
+		current = current->next;
+		free(tmp);
+	}
+	head = NULL;
+}
