676e966881fce54ad92da66af7bbb9964ea51e88
mokasp <kasperbouldin@gmail.com>
2023-07-16 23:01:49 -0700

    whitespace

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	int new_int;
 	
 	new_int = n;
-	
 	new_node = (listint_t *) malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
