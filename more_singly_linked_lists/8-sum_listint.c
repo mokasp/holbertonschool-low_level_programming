b89d7799520a26296ee8fa6c4740654631834934
mokasp <kasperbouldin@gmail.com>
2023-07-17 19:11:49 -0700

    Intial file upload

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * um_listint - finds sum of all data of a singly linked list
+ * head: pointer to first element
+ *
+ * Return: sum of all data
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr;
+	int sum;
+
+	sum = 0;
+
+	while (ptr != NULL)
+	{i
+		sum += ptr->n;
+		prt = ptr->next;
+	}
+	return (sum);
+}
