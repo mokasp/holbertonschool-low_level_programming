a9206915a1e6453a3e185123a7f6ab484aaa4a63
mokasp <kasperbouldin@gmail.com>
2023-07-18 15:50:35 -0700

    typos

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,15 +9,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, fd, wr;
-	char buf[1024];
+	int file_from, file_to, rd, wr;
+	char buff[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(arg[1], O_RDONLY);
+	file_from = open(argv[1], O_RDONLY);
 
 	rd = read(file_from, buff, 1024);
 	if (rd == -1)
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", args[1]);
 		exit(98);
 	}
-	file_to = open(arg[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR);
 	wr = write(file_to, buff, rd);
 	if (wr == -1)
 	{
@@ -36,8 +36,7 @@ int main(int argc, char *argv[])
 	close(file_to);
 	if (file_from == -1 || file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", args[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(100);
 	}
-	close(file_to);
 }
