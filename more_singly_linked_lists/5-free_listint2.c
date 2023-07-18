ddeaa6dc5907620a34502b6bf7b0988c005581d2
mokasp <kasperbouldin@gmail.com>
2023-07-17 20:26:53 -0700

    freed head

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -12,8 +12,10 @@ void free_listint2(listint_t **head)
 	listint_t *tmp;
 
 	if ((*head) == NULL)
+		free(*head);
 		return;
 	if (head == NULL)
+		free(*head);
 		return;
 	while (*head)
 	{
@@ -21,5 +23,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		(*head) = tmp;
 	}
-	head = NULL;
+	(*head) = NULL;
 }
