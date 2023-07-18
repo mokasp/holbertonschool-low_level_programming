dcb43ac68d2ae18c42448fe0421bb0ed80bccc49
mokasp <kasperbouldin@gmail.com>
2023-07-18 12:53:23 -0700

    initialize cnt

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,6 +11,8 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, wr, cnt;
 
+	cnt = 0;
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
