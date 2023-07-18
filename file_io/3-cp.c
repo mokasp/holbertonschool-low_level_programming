401ddc849f04075febc6b74765e1de649da7730a
mokasp <kasperbouldin@gmail.com>
2023-07-18 15:48:40 -0700

    testing

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, fd, wr, rd;
+	int file_from, file_to, fd, wr;
 	char buf[1024];
 
 	if (argc != 3)
@@ -17,12 +17,10 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	/* if file descriptor cant close */
-
 	file_from = open(arg[1], O_RDONLY);
 
 	rd = read(file_from, buff, 1024);
-	if (file_from == -1)
+	if (rd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", args[1]);
 		exit(98);
@@ -34,6 +32,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", args[1]);
 		exit(99);
 	}
-	
-
+	close(file_from);
+	close(file_to);
+	if (file_from == -1 || file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", args[1]);
+		exit(100);
+	}
+	close(file_to);
 }
