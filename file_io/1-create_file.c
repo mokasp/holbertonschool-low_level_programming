719cd8e4932789ca105e6ab3e5cb6be858cbd134
mokasp <kasperbouldin@gmail.com>
2023-07-17 21:10:04 -0700

    typo

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, wr, cnt;
 
-	if (file == NULL)
+	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (text_content)
