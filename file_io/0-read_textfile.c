7598b939de5b3d61ee7819a1c55cd29b50f39029
mokasp <kasperbouldin@gmail.com>
2023-07-17 20:59:15 -0700

    reformat

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -15,25 +15,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	str = malloc(sizeof(char) * letters);
-	if (str == NULL)
+	fd = open("filename", O_RDONLY);
+	if (fd == -1)
 		return (0);
 	
-	if (filename != NULL)
+	str = malloc(sizeof(char) * letters);
+	if (str == NULL)
 	{
-		fd = open("filename", O_RDONLY);
-		if (fd == -1)
-			return (0);
+		free(str);
+		return (0);
 	}
-
+	
 	rd = read(fd, str, letters);
 	if (rd == -1)
 	{
 		return (0);
 	}
-	wr = write(STDOUT_FILENO, str, letters);
-	if (wr == -1)
+	wr = write(STDOUT_FILENO, str, rd);
+	if (wr == -1 || rd != wr)
 		return (0);
+	free(str);
 	close(fd);
 	return (wr);
 }
---
+++ file_io/1-create_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: text content to put in file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+ssize_t create_file(const char *filename, size_t letters)
+{
+	
+}
