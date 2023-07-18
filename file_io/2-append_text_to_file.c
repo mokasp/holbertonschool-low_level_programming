37d27e4a35366e27f414b7ae02163de4d71cc084
mokasp <kasperbouldin@gmail.com>
2023-07-18 12:11:52 -0700

    added libraries

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: text content to put in file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr, cnt;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content)
+	{
+		for (cnt = 0; text_content[cnt] != '\0'; cnt++)
+		{
+		}
+		wr = write(fd, text_content, cnt);
+		if (wr == -1)
+		{
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
