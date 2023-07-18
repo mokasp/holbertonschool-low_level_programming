b0caec16dafb4d1b45990dba2f788ef763794e65
mokasp <kasperbouldin@gmail.com>
2023-07-17 21:08:27 -0700

    initial file commit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,7 +7,24 @@
  *
  * Return: 1 on success and -1 on failure
  */
-ssize_t create_file(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content);
 {
-	
+	int fd, wr, cnt;
+
+	if (file == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content)
+	{
+		for (cnt = 0; text_content[cnt] != '\0'; cnt++)
+		{
+		}
+		wr = write(fd, text_content, cnt);
+		if (wr == -1)
+		{
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
