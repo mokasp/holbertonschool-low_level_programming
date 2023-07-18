0691d131689dcf733e359c6fdd2ed20b8a4aa200
mokasp <kasperbouldin@gmail.com>
2023-07-17 17:03:55 -0700

    edited write call

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -29,7 +29,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	wr = write(fd, str, letters);
+	wr = write(STDOUT_FILENO, str, letters);
 	if (wr == -1)
 		return (0);
 	close(fd);
