7a8bd6148e3b75fa699f6c45ea096e5b20f9efc3
mokasp <kasperbouldin@gmail.com>
2023-07-18 13:54:12 -0700

    changed bracket position

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,11 +18,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		while (text_content[cnt] != '\0')
 			cnt++;
-		wr = write(fd, text_content, cnt);
-		if (wr == -1)
-			return (-1);
-
 	}
+	wr = write(fd, text_content, cnt);
+	if (wr == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
