8c450a5c0afd1afd892377ed87fb4dfd9f79acf5
mokasp <kasperbouldin@gmail.com>
2023-07-18 21:07:22 -0700

    fjbnmb

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,8 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *tmp;
+	listint_t *new_node;
+	listint_t *tmp;
 	unsigned int new_int;
 
 	new_int = n;
