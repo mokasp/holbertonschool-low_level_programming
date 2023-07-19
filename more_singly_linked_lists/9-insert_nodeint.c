0a8777f9cd2fd29fbc3e7fb9dc9b7db5d54cea76
mokasp <kasperbouldin@gmail.com>
2023-07-18 21:05:43 -0700

    removed variable

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current, *tmp;
+	listint_t *new_node, *tmp;
 	unsigned int new_int;
 
 	new_int = n;
