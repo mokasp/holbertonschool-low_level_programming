baedf5bbe3d0eab090389caf8f4fd10778ddc973
mokasp <kasperbouldin@gmail.com>
2023-07-18 21:04:32 -0700

    edge case

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -11,28 +11,18 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *current, *tmp;
-	unsigned int new_int, cnt;
+	unsigned int new_int;
 
 	new_int = n;
-	cnt = 0;
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	current = (*head);
-	while (current != NULL)
+	if (idx == 0)
 	{
-		current = current->next;
-		cnt++;
-	}
-	if (idx < 1 || idx > cnt + 1)
-		return (NULL);
-	else if (idx == 0)
-	{
-		new_node->next = *head;
+		new_node->next = tmp;
 		*head = new_node;
+		return (new_node);
 	}
-	else
-		tmp = *head;
 	while (--idx)
 		tmp = tmp->next;
 	new_node->n = new_int;
