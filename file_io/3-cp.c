c3837f460e6c96f9f32c14ac57eac304f4bd9b88
mokasp <kasperbouldin@gmail.com>
2023-07-18 17:29:51 -0700

    error handle

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
 
