7c59003a9bb836b0a227581901e0b6b8e6183f12
mokasp <kasperbouldin@gmail.com>
2023-07-18 20:56:05 -0700

    newint

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -32,9 +32,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 	}
 	else
-		*tmp = *head;
+		tmp = *head;
 	while (--idx)
 		tmp = tmp->next;
+	new_node->n = new_int;
 	new_node->next = tmp->next;
 	tmp->next = new_node;
 	return (new_node);
