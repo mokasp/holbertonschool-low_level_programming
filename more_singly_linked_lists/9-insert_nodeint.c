6356dc070f766a77d61c6e49e6c4e89930748386
mokasp <kasperbouldin@gmail.com>
2023-07-18 20:44:29 -0700

    vvariables

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -15,14 +15,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_int = n;
 	cnt = 0;
-	new_node = (listint_t *)malloc(sizeof(list_int));
+	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	current = (*head);
 	while (current != NULL)
 	{
 		current = current->next;
-		cnt++
+		cnt++;
 	}
 	if (idx < 0 || idx > cnt)
 		return (NULL);
