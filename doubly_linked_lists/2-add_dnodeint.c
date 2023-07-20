eb6dd0bc86ecdfb7f7cb459bf444ee8bc4b74cb7
mokasp <kasperbouldin@gmail.com>
2023-07-20 11:28:49 -0700

    added pointer

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -33,8 +33,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		head = new_node;
 		return (new_node);
 	}
-	head->prev = new_node;
+	(*head)->prev = new_node;
 	new_node->next = (*head);
-	*head = new_node;
+	(*head) = new_node;
 	return (new_node);
 }
