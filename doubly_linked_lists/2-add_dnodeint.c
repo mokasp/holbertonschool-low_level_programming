e891a54cf5b7fb8646670b267b982b2da1c1f1eb
mokasp <kasperbouldin@gmail.com>
2023-07-20 11:25:28 -0700

    TYPO

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -28,12 +28,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = getnewnode(n);
 
-	if (head == NULL)
+	if (h == NULL)
 	{
-		head = new_node;
+		h = new_node;
 		return (new_node);
 	}
-	head->prev = new_node;
+	h->prev = new_node;
 	new_node->next = head;
 	head = new_node;
 	return (new_node);
