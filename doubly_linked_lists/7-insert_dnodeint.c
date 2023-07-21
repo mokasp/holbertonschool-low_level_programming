9c55e3d947510c8eb045ee614e2a5310e8de6524
mokasp <kasperbouldin@gmail.com>
2023-07-21 12:21:15 -0700

    error handle

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -56,8 +56,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		add_dnodeint_end(h, n);
 	else if (*h == NULL)
 	{
-		*h = getnewnode3(n);
-		return (*h);
+		new_node = getnewnode3(n);
+		*h = new_node);
+		return (new_node);
 	}
 	else
 	{
