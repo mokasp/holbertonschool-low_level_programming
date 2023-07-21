daada564ad4a60de9f500383d9e5fb7c14776fe2
mokasp <kasperbouldin@gmail.com>
2023-07-21 10:58:11 -0700

    Initial file commit

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * *get_dnodeint_at_index - get node at nth index
+ * @head: pointer to pointer to head of doubly linked list
+ * @index: position
+ *
+ * Return: address of nth element on success, NULL if failed
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int cnt;
+
+	tmp = head;
+	if (head == NULL)
+		return (NULL);
+	for (cnt = 0; head->next; cnt++)
+	{
+		if (cnt == index)
+			return (tmp);
+	}
+	return (NULL);
+}
