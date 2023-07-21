193db5ae63cbb9392997bc7217d5c7e956d50723
mokasp <kasperbouldin@gmail.com>
2023-07-21 12:01:57 -0700

    initial file commit

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,70 @@
+#include "lists.h"
+/**
+ * getnewnode - create a new node
+ * @data: data to put in node
+ *
+ * Return: address of new element
+ */
+dlistint_t *getnewnode(int data)
+{
+	dlistint_t *new_node;
+
+	new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = data;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
+ * getSize - create a new node
+ * @node: node to get size of
+ *
+ * Return: size of dll
+ */
+int getSize(dlistint_t *node)
+{
+	int size = 0;
+
+	while (node != NULL)
+	{
+		node = node->next;
+		size++;
+	}
+	return (size);
+}
+/**
+ * *insert_dnodeint_at_index - add node at index of doubly linked list
+ * @h: pointer to pointer to head of doubly linked list
+ * @n: data for node
+ * @idx: position
+ *
+ * Return: address of new element on success, NULL if failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	int size = getSize(*h);
+	dlistint_t *new_node, *tmp, *tmp2;
+
+	if (idx < 0 || size < idx)
+		return (NULL);
+	if (idx == size)
+		add_dnodeint(h, n);
+	else if (idx == size)
+		add_dnodeint_end(h, n);
+	else
+	{
+		tmp = *head;
+		new_node = getnewnode(n);
+		while (--idx)
+			tmp = tmp->next;
+		tmp2 = tmp->next;
+		new_node->next = tmp->next;
+		new_node->prev = tmp;
+		tmp->next = new_node;
+		tmp2->prev = new_node;
+	}
+	return (new_node);
+}
