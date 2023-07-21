628f672c70486d02cf33902e063e4a4df7b5dafc
mokasp <kasperbouldin@gmail.com>
2023-07-21 11:30:49 -0700

    initial file commit

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_dlistint - get node at nth index
+ * @head: pointer to pointer to head of doubly linked list
+ *
+ * Return: address of nth element on success, NULL if failed
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+	int sum;
+
+	tmp = head;
+	cnt = 0;
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
+}
