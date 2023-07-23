26e847112bfeeda8e9efcaab0fcc1af61b168037
mokasp <kasperbouldin@gmail.com>
2023-07-23 13:21:16 -0700

    added createnode  func

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,26 @@
 #include "hash_tables.h"
 
 /**
+ * *createNode - allocate memory for nodes
+ * @key: key/string
+ * @value: value corresponding to a key
+ *
+ * Return: pointer to node
+ */
+hash_node_t *createNode(char *key, char *value)
+{
+	hash_node_t *node = (hash_node_t *) malloc(sizeof(hash_node_t));
+
+	if (node == NULL)
+		return (NULL);
+	node->key = (char *) malloc(strlen(key) + 1);
+	node->value = (char *) malloc(strlen(key) + 1);
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+	return (node);
+}
+
+/**
  * hash_table_set - adds an element to hash table
  * @ht hash table modify
  * @key: key
