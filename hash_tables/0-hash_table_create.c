ca0c93285279e170588e7bf8989780e8f93e6782
mokasp <kasperbouldin@gmail.com>
2023-07-22 17:52:39 -0700

    handle malloc return

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,6 +10,8 @@
 hash_node_t *createNode(char *key, char *value)
 {
 	hash_node_t *node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
 	node->key = (char *) malloc(strlen(key) + 1);
 	node->value = (char *) malloc(strlen(key) + 1);
 	strcpy(node->key, key);
@@ -26,6 +28,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (node == NULL)
+		return (NULL);
 	table->size = size;
 	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
 	for (i = 0; i < table->size; i++)
