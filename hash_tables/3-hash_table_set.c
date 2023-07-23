8e4d5f5caeda617f1c54af538f69f5f8b1204d1c
mokasp <kasperbouldin@gmail.com>
2023-07-23 14:11:57 -0700

    sign

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ hash_node_t *createNode(const char *key, const char *value)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node = createNode(key, value);
-	int index = key_index(key);
+	usigned long index = key_index(key);
 	hash_node_t *currentNode = ht->array[index];
 
 	ht->array[index] = node;
