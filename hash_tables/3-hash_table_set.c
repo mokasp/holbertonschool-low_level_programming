8cd316992ef9e928aa004d339095e6a64a935314
mokasp <kasperbouldin@gmail.com>
2023-07-23 14:02:26 -0700

    TYO

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,8 +13,8 @@ hash_node_t *createNode(const char *key, const char *value)
 
 	if (node == NULL)
 		return (NULL);
-	node->key = (const char *) malloc(strlen(key) + 1);
-	node->value = (const char *) malloc(strlen(key) + 1);
+	node->key = (char *) malloc(strlen(key) + 1);
+	node->value = (char *) malloc(strlen(key) + 1);
 	strcpy(node->key, key);
 	strcpy(node->value, value);
 	return (node);
@@ -30,9 +30,9 @@ hash_node_t *createNode(const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t node = createNode(key, value);
+	hash_node_t *node = createNode(key, value);
 	int index = key_index(key);
-	hash_node_t currentNode = ht->array[index];
+	hash_node_t *currentNode = ht->array[index];
 
 	ht->array[index] = node;
 	return (1);
