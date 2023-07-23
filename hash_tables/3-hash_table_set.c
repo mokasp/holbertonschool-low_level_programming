b5e66cd5fdc578a7e360443f7224c6d236cf6b31
mokasp <kasperbouldin@gmail.com>
2023-07-23 14:00:13 -0700

    chang3ed types

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -7,14 +7,14 @@
  *
  * Return: pointer to node
  */
-hash_node_t *createNode(const unsigned char *key, const char *value)
+hash_node_t *createNode(const char *key, const char *value)
 {
 	hash_node_t *node = (hash_node_t *) malloc(sizeof(hash_node_t));
 
 	if (node == NULL)
 		return (NULL);
-	node->key = (const unsigned char *) malloc(strlen(key) + 1);
-	node->value = (char *) malloc(strlen(key) + 1);
+	node->key = (const char *) malloc(strlen(key) + 1);
+	node->value = (const char *) malloc(strlen(key) + 1);
 	strcpy(node->key, key);
 	strcpy(node->value, value);
 	return (node);
@@ -28,7 +28,7 @@ hash_node_t *createNode(const unsigned char *key, const char *value)
  *
  * Return: 1 on success, 0 on failure
  */
-int hash_table_set(hash_table_t *ht, const unsigned char *key, const char *value)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t node = createNode(key, value);
 	int index = key_index(key);
