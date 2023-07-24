e82134a6e7c3563871b5fac0c40121ec4efd6d2d
mokasp <kasperbouldin@gmail.com>
2023-07-24 09:22:51 -0700

    seperator

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -15,8 +15,10 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		if (i < ht->size - 1)
 			printf("'%s': '%s', ", ht->array[i]->key, ht->array[i]->value);
+		else if (i < ht->size)
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value;
 	}
 	printf("}\n");
 }
