a80fea3b19974a34b846092a177a98df7097ad4e
mokasp <kasperbouldin@gmail.com>
2023-07-24 08:03:16 -0700

    changed type

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int slot;
-	hash_table_t *node;
+	hash_node_t *node;
 	if (node == NULL)
 		return (NULL);
 	while (node != NULL)
