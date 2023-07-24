f46d67c79f9eeff0a2fb1d9bd688d5771ef78486
mokasp <kasperbouldin@gmail.com>
2023-07-24 12:28:37 -0700

    white space

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -24,7 +24,7 @@ void hash_table_print(const hash_table_t *ht)
 			while (tmp != NULL)
 			{
 				if (f == 1)
-					printf(", ");		
+					printf(", ");
 				printf("'%s': '%s'", tmp->key, tmp->value);
 				tmp = tmp->next;
 				f = 1;
