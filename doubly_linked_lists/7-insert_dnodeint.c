32d11ad03136cd6485fa8db5daeff6cd716d2bd0
mokasp <kasperbouldin@gmail.com>
2023-07-24 13:10:04 -0700

    testin

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -55,23 +55,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	if (size < idx || idx == 0)
+	if (size < idx)
 		return (NULL);
-	if (idx == size)
-		add_dnodeint(h, n);
 	else if (idx == size)
 		add_dnodeint_end(h, n);
 	else
 	{
 		tmp = *h;
 		new_node = getnewnode3(n);
-		while (--idx)
-			tmp = tmp->next;
-		tmp2 = tmp->next;
-		new_node->next = tmp->next;
-		new_node->prev = tmp;
-		tmp->next = new_node;
-		tmp2->prev = new_node;
+		while (tmp)
+		{
+			if (i < idx)
+			{
+				tmp2 = tmp;
+				tmp = tmp->next;
+			}
+			i++;
+		}
+
+		if (tmp != NULL)
+		{
+			new_node->next = tmp->next;
+			new_node->prev = tmp;
+			tmp->next = new_node;
+			tmp->prev = new_node;
+		}
 	}
 	return (new_node);
 }
