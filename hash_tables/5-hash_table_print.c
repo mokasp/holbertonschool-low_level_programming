c5b0d3cf0326ed7202906461dfa9c5e0ce1bdd55
mokasp <kasperbouldin@gmail.com>
2023-07-24 09:13:23 -0700

    initial file commit

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table modify
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht[i]) != NULL)
+			printf("%s: %s ", ht[i]->key, ht[i]->value);
+		else
+			printf("");
+	}
+}
