0e042c2095f6617fb886fd580fcfa54b66a7a04f
mokasp <kasperbouldin@gmail.com>
2023-07-24 09:34:58 -0700

    forgot bracket

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -21,6 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 				printf("'%s': '%s', ", ht->array[i]->key, ht->array[i]->value);
 			else
 				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+		}
 	}
 	printf("}\n");
 }
