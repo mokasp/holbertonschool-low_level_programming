7d70b8c8eb1a8c2f13b06079ba2585463072bd65
mokasp <kasperbouldin@gmail.com>
2023-07-24 09:28:08 -0700

    sign typo

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -17,7 +17,7 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		if (i < (ht->size - 1) && ht->array[i] != NULL)
 			printf("'%s': '%s', ", ht->array[i]->key, ht->array[i]->value);
-		else if (i => (ht->size - 1)_ && i < ht->size && ht->array[i] != NULL)
+		else if (i >= (ht->size - 1)_ && i < ht->size && ht->array[i] != NULL)
 			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 	}
 	printf("}\n");
