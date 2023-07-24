574c1221ac7c211f951f734fbf86b4cd72504dde
mokasp <kasperbouldin@gmail.com>
2023-07-24 08:01:06 -0700

    intial test file

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table modify
+ * @key: key
+ *
+ * Return: value associated with key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int slot;
+	hash_table_t *node;
+	if (node == NULL)
+		return (NULL);
+	while (node != NULL)
+	{
+		if (strcom(node->key, key) == 0)
+			return (node->value);
+		node = node->value;
+	}
+	return (NULL);
+}
