54c443e2098927985f0a269688d6b0f39d29c50b
mokasp <kasperbouldin@gmail.com>
2023-07-24 19:43:14 -0700

    njovrj

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -53,7 +53,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	if (idx == size)
+	if (idx > size)
 		return (NULL);
 
 	else
