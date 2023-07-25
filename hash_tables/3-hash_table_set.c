dfd5bc258ece12a5f7fd0602d0c74615895a93ad
mokasp <kasperbouldin@gmail.com>
2023-07-24 19:33:30 -0700

    free

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -23,6 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node = ht->array[slot];
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
+		free(newNode);
 		return (0);
 	newNode = createNode(key, value);
 	if (ht->array[slot] == NULL)
