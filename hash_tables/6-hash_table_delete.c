fc4cefa0b042430e022aad66450490507f337611
mokasp <kasperbouldin@gmail.com>
2023-07-24 18:29:17 -0700

    rearranged

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -16,13 +16,13 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
-		while (node != NULL)
+		while (node)
 		{
-			tmp = node;
 			node = node->next;
 			free(tmp->key);
 			free(tmp->value);
 			free(tmp);
+			node = tmp;
 		}
 	}
 	free(ht->array);
