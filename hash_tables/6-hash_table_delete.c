b459af1312029671c10a178f779147b1fdcb1615
mokasp <kasperbouldin@gmail.com>
2023-07-24 18:19:19 -0700

    new resource

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -9,19 +9,19 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *node;
+	hash_node_t *node, tmp;
 
 	if (ht == NULL)
 		return;
 	for (i = 0; i < ht->size; i++)
 	{
-		while (ht->array[i] != NULL)
+		node = ht->array[i];
+		while (node != NULL)
 		{
-			node = ht->array[i]->next;
-			free(ht->array[i]->key);
-			free(ht->array[i]->value);
-			free(ht->array[i]);
-			ht->array[i] = node;
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp);
 		}
 	}
 	free(ht->array);
