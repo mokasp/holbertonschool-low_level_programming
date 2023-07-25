bee6fd4d27ceef67cf4b0be634cccba29a7b3c2e
mokasp <kasperbouldin@gmail.com>
2023-07-24 18:32:40 -0700

    typos

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -20,10 +20,10 @@ void hash_table_delete(hash_table_t *ht)
 		node = ht->array[i];
 		while (node)
 		{
-			node = node->next;
-			free(tmp->key);
-			free(tmp->value);
-			free(tmp);
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
 			node = tmp;
 		}
 	}
