2e2eb05b6e7cadb11f9e0adda1b2ad497653c41d
mokasp <kasperbouldin@gmail.com>
2023-07-24 17:24:53 -0700

    handle null error

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -11,6 +11,8 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *node;
 
+	if (ht == NULL)
+		return;
 	for (i = 0; i < ht->size; i++)
 	{
 		while (ht->array[i] != NULL)
