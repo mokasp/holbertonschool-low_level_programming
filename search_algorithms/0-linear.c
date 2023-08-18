6750dc7e1ade2a9c25a9c6662bb55c06ff63eb1e
mokasp <kasperbouldin@gmail.com>
2023-08-18 13:23:14 -0700

    fixed comparison

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,12 +6,14 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array != NULL)
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 			if (array[i] == value)
 				return (array[i]);
 		}
+		return (-1);
 	}
+
 	return (-1);
 }
