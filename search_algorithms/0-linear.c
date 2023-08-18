fc16a70241e848c510c661e76d5b447e8113f29d
mokasp <kasperbouldin@gmail.com>
2023-08-18 13:51:48 -0700

    betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - searches for first occurance of a value in an array
+ * @array: array to search through
+ * @size: size of array
+ * @value: value to search for
+ *
+ * Return: On success, the index of the value found. On failure, -1
+ */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
