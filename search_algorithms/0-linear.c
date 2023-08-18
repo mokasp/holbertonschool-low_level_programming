4fa996ae614e7fafd88ae14a0e40b3d0445ffab6
mokasp <kasperbouldin@gmail.com>
2023-08-18 13:48:03 -0700

    edited return

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,7 +10,7 @@ int linear_search(int *array, size_t size, int value)
 		{
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 			if (array[i] == value)
-				return (array[i]);
+				return (i);
 		}
 		return (-1);
 	}
